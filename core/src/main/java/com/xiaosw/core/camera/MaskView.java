6c574cde90d0e774f0ecb37a9a347a63c73ffbd6
xiaoshiwang <xiaoshiwang@yishu.com>
2018-03-16 18:58:36 +0800

    裁剪camera指定区域图形

---
+++ core/src/main/java/com/xiaosw/core/camera/MaskView.java
@@ -0,0 +1,11 @@
+package com.xiaosw.core.camera;
+
+/**
+ * @ClassName {@link MaskView}
+ * @Description
+ * @Date 2018-03-16.
+ * @Author xiaosw<xiaosw0802@163.com>.
+ */
+
+public class MaskView {
+}
